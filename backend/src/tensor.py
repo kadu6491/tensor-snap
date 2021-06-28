704bfeb947e23d066945cdf5d73ed8d2969048db
Kofi Adutwum <kadut001@odu.edu>
2021-06-28 14:55:04 -0400

    tensorflow-text install added

--- backend/src/tensor.py
+++ backend/src/tensor.py
@@ -1,6 +1,6 @@
-import numpy as np
-import tensorflow as tf
-import tensorflow_text as tf_text
+# import numpy as np
+# import tensorflow as tf
+# import tensorflow_text as tf_text
 # from keras.preprocessing import image
 # from keras.applications import vgg19
 #
@@ -21,19 +21,19 @@ import tensorflow_text as tf_text
 # for id, name, lh in prediction_classes[0]:
 #     print("Prediction: {} - {:2f}".format(name, lh))
 
-three_input_text = tf.constant([
-     # 'Esta es mi vida.',
-    'This is my life.',
-    # '¿Todavía están en casa?',
-    'Are they still home?',
-    # 'Tratar de descubrir.',
-    'Try to find out.',
-])
-
-reloaded = tf.saved_model.load('../models/translator')
-
-result = reloaded.tf_translate(three_input_text)
-for tr in result['text']:
-  print(tr.numpy().decode())
+# three_input_text = tf.constant([
+#      # 'Esta es mi vida.',
+#     'This is my life.',
+#     # '¿Todavía están en casa?',
+#     'Are they still home?',
+#     # 'Tratar de descubrir.',
+#     'Try to find out.',
+# ])
+#
+# reloaded = tf.saved_model.load('../models/translator')
+#
+# result = reloaded.tf_translate(three_input_text)
+# for tr in result['text']:
+#   print(tr.numpy().decode())
 
 # print()

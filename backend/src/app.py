a562568abd0514f1ca57b7a8fa7694dbab538789
Kofi Adutwum <kadut001@odu.edu>
2021-06-28 14:28:36 -0400

    set up docker file and others

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,12 +1,73 @@
+from flask import *
 from PyDictionary import PyDictionary
-import goslate
+from translate import Translator
 
-gs = goslate.Goslate()
-dictionary=PyDictionary("hotel","ambush","nonchalant","perceptive")
-'There can be any number of words in the Instance'
+# import numpy as np
+# from keras.preprocessing import image
+# from keras.applications import vgg19
 
-print(dictionary.printMeanings())
-print(dictionary.getMeanings())
-print (dictionary.getSynonyms())
 
-print (dictionary.translateTo("hi"))
+app = Flask(__name__);
+
+dictionary = PyDictionary()
+translator = Translator(to_lang='es')
+fr_trans = Translator(to_lang='fr')
+
+
+def definition(word):
+    return dictionary.meaning(word)
+
+
+# def img_classifier(imgs):
+#     model = vgg19.VGG19(weights='imagenet')
+#
+#     img = image.load_img(f"../{imgs}", target_size=(224, 224))
+#
+#     x = image.img_to_array(img)
+#     x = np.expand_dims(x, axis=0)
+#     x = vgg19.preprocess_input(x)
+#
+#     prediction = model.predict(x)
+#     prediction_classes = vgg19.decode_predictions(prediction, top=4)
+#
+#     feat_name = []
+#
+#     for id, name, lh in prediction_classes[0]:
+#         # print("Prediction: {} - {:2f}".format(name, lh))
+#         feat_name.append(name.replace('_', ' '))
+#
+#     return feat_name
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Welcome to translation home"
+
+
+@app.route('/<word>/', methods=['GET'])
+def translate(word):
+    # img = img_classifier(word)
+    w = word.split('.')[0]
+    d = definition(w)
+    s = translator.translate(w)
+    f = fr_trans.translate(w)
+
+    spa_key = []
+    spa_def = []
+
+    # s_d = translator.translate(d[0][0])
+    print(spa_def)
+    return render_template(
+        'index.html',
+        word=word.capitalize(),
+        e=w.capitalize(),
+        d=d,
+        s=s.capitalize(),
+        f=f.capitalize(),
+        img=["Hi", "many"]
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
---
+++ backend/src/tensor.py
@@ -0,0 +1,39 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_text as tf_text
+# from keras.preprocessing import image
+# from keras.applications import vgg19
+#
+# model = vgg19.VGG19(weights='imagenet')
+#
+# img = image.load_img("../rose1.jpeg", target_size=(224, 224))
+#
+# x = image.img_to_array(img)
+#
+# x = np.expand_dims(x, axis=0)
+#
+# x = vgg19.preprocess_input(x)
+#
+# prediction = model.predict(x)
+#
+# prediction_classes = vgg19.decode_predictions(prediction, top=9)
+#
+# for id, name, lh in prediction_classes[0]:
+#     print("Prediction: {} - {:2f}".format(name, lh))
+
+three_input_text = tf.constant([
+     # 'Esta es mi vida.',
+    'This is my life.',
+    # '¿Todavía están en casa?',
+    'Are they still home?',
+    # 'Tratar de descubrir.',
+    'Try to find out.',
+])
+
+reloaded = tf.saved_model.load('../models/translator')
+
+result = reloaded.tf_translate(three_input_text)
+for tr in result['text']:
+  print(tr.numpy().decode())
+
+# print()
